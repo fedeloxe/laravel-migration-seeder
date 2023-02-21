ca52832cf61faa9b1223c18827993f2339d22bed
fedeloxe <loxeee@gmail.com>
2023-02-21 14:51:58 +0100

    faker done

--- database/migrations/2023_02_21_124139_create_trains_table.php
+++ database/migrations/2023_02_21_124139_create_trains_table.php
@@ -16,6 +16,15 @@ return new class extends Migration
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('azienda', 50);
+            $table->string('stazione_di_partenza', 50);
+            $table->string('Stazione_di_arrivo', 50);
+            $table->string('orario_di_partenza', 50);
+            $table->string('Orario_di_arrivo', 50);
+            $table->smallInteger('Codice_Treno')->unsigned();
+            $table->smallInteger('Numero_Carrozze')->unsigned();
+            $table->boolean('In_orario');
+            $table->boolean('Cancellato');
         });
     }
 
---
+++ database/seeders/TrainSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Train;
+use App\Models\Train as ModelsTrain;
+
+class TrainSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i <= 10; $i++) {
+
+            $newTrain = new Train();
+            $newTrain->azienda = $faker->word();
+            $newTrain->stazione_di_partenza = $faker->word();
+            $newTrain->Stazione_di_arrivo = $faker->word();
+            $newTrain->orario_di_partenza = $faker->word();
+            $newTrain->Orario_di_arrivo = $faker->word();
+            $newTrain->Codice_Treno = $faker->word();
+            $newTrain->Numero_Carrozze = $faker->numberBetween(1, 50);
+            $newTrain->In_orariod = $faker->randomElement([true, false]);
+            $newTrain->Cancellato = $faker->randomNumber([true, false]);
+            $newTrain->save();
+        }
+    }
+}
